b6d6053e54a8e4d2d83963acf8d92ebac5f791ce
Luong Huan <huandt08@gmail.com>
2023-07-25 22:37:13 +0700

    Refine sync cart function

--- src/main/java/com/beetech/finalproject/domain/repository/CartRepository.java
+++ src/main/java/com/beetech/finalproject/domain/repository/CartRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface CartRepository extends CrudRepository<Cart, Long>, ListCrudRepository<Cart, Long> {
-    Cart findByToken(String token);
+    Optional<Cart> findByToken(String token);
 }
